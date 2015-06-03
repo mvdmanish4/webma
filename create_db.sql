0a4e1410ddb64fcf491c413f1f5e306555ed2224
Arushi Jain <arushij@stanford.edu>
2015-06-03 20:58:44 +0530

    hospitals tsv

--- create_db.sql
+++ create_db.sql
@@ -3,51 +3,36 @@ CREATE DATABASE IF NOT EXISTS Khojo;
 
 CREATE TABLE Hospital
 (
-HospitalID int,
-Name varchar(255),
-Address varchar(255),
-City varchar(255),
-Pincode int, 
+HospitalID int NOT NULL AUTO_INCREMENT,
+Name varchar(255) NOT NULL,
+Address varchar(255)  NOT NULL,
 Phone int, 
 Latitude float, 
 Longitude float, 
-Tier int
 );
 
 
 CREATE TABLE Clinic
 (
-ClinicID int,
-Name varchar(255),
+ClinicID int NOT NULL AUTO_INCREMENT,
+Name varchar(255) NOT NULL,
 Address varchar(255),
-City varchar(255),
-Pincode int, 
 Phone int, 
 Latitude float, 
 Longitude float, 
-Tier int
 );
 
 
 CREATE TABLE Pharmacy
 (
-PharmacyID int,
-Name varchar(255),
+PharmacyID int NOT NULL AUTO_INCREMENT,
+Name varchar(255)  NOT NULL,
 Address varchar(255),
-City varchar(255),
-Pincode int, 
 Phone int, 
 Latitude float, 
 Longitude float, 
-Tier int
 );
 
 
 
-LOAD DATA INFILE 'c:/tmp/discounts.csv' 
-INTO TABLE discounts 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
 
